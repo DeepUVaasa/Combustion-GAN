566563ea9e6f2c1205c276691b73682d0bb93887
DeepUVaasa <117087160+DeepUVaasa@users.noreply.github.com>
2026-07-13 14:08:05 +0300

    Add files via upload

---
+++ ISCP/run_iscp.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+cd experiments/iscp
+
+test=1    # 0 means train the model, 1 means evaluate the model
+threshold=0.02
+fold_cnt=1
+dataroot="./dataset/" 
+model="CombustionGAN"
+
+w_adv=1
+niter=200
+lr=0.0001
+n_aug=0
+
+outf="./output"
+
+for (( i=0; i<$fold_cnt; i+=1))
+do
+    echo "#################################"
+    echo "########  Folder $i  ############"
+    if [ $test = 0 ]; then
+	    python -u main.py  \
+            --dataroot $dataroot \
+            --model $model \
+            --niter $niter \
+            --lr $lr \
+            --outf  $outf \
+            --folder $i
+
+	else
+	    python -u main.py  \
+            --dataroot $dataroot \
+            --model $model \
+            --niter $niter \
+            --lr $lr \
+            --outf  $outf \
+            --folder $i  \
+            --outf  $outf \
+            --istest  \
+            --threshold $threshold
+    fi
+
+done
